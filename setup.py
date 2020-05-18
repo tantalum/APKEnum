84f550364b642cf8598acd6d3790f88fef8479dd
Nabil Alsharif <blit32@circuitsofimagination.com>
2020-05-18 14:34:55 -0500

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='APK Enum',
+      version='0.1',
+      description='A tool to enumerating interesting nuggts from APK files',
+      author='Nabil Alsharif',
+      author_email='blit32@circuitsofimagination.com',
+      url='https://github.com/tantalum/APKEnum',
+      license='GPL',
+      scripts=['apkenum']
+     )
